afae2592340d2b376af3f86d199e127ded8bc574
Sakura7383 <cherli3c33@gmail.com>
2024-07-30 18:32:59 -0500

    Cleaned up

--- src/js/controller.js
+++ src/js/controller.js
@@ -148,10 +148,6 @@ const controlAddRecipe = async function (newRecipe) {
   }
 };
 
-const newFeature = function () {
-  console.log('Welcome to the application');
-};
-
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
 
@@ -162,7 +158,6 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
-  newFeature();
 };
 
 init();
